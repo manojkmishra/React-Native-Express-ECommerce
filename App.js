5d4670398d1e81a40b9d8e46d47e7f7e710e64f8
manojkmishra <manojmishra571980@gmail.com>
2018-08-14 15:08:36 +1000

    utilitytheme imported in app.js

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StyleSheet, Text, View } from 'react-native';
+import { UtilityThemeProvider, Box, Text } from 'react-native-design-utility';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <UtilityThemeProvider>
+      <Box f={1} center >
         <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      </Box>
+      </UtilityThemeProvider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({ container: { flex: 1, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center',  },});
