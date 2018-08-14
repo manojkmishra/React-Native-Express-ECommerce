73ad9eb306c68945c6e4770eb9a70fcb7931b08e
manojkmishra <manojmishra571980@gmail.com>
2018-08-15 09:45:30 +1000

    navigation created[SplashScreen,HomeScreen,LoginScreen]

--- App.js
+++ App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
 import { UtilityThemeProvider, Box, Text } from 'react-native-design-utility';
+import Navigation from './src/screens';
 
 export default class App extends React.Component {
   render() {
     return (
       <UtilityThemeProvider>
-      <Box f={1} center >
-        <Text>Open up App.js to start working on your app!</Text>
-      </Box>
+        <Navigation/>
       </UtilityThemeProvider>
     );
   }
 }
-
-// const styles = StyleSheet.create({ container: { flex: 1, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center',  },});
