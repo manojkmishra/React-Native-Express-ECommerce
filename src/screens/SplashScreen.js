b5acad2d9e7e8d17cdf72c54b4deb6bb75493490
manojkmishra <manojmishra571980@gmail.com>
2018-08-15 10:02:43 +1000

    after one second- navigates to LoginScreen--from SplashScreen

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -3,6 +3,8 @@ import { Box, Text } from 'react-native-design-utility';
 
 class SplashScreen extends Component 
 {   state={};
+    componentDidMount() {   this.checkAuth();  }
+    checkAuth = () => {    setTimeout(() => {  this.props.navigation.navigate('Auth');  }, 1000);    };
     render() 
     {   return (
         <Box f={1} center >
