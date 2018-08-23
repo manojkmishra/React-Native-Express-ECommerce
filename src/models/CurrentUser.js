52bb41e887260baf78d8d29cf73e75e8d81e545b
manojkmishra <manojmishra571980@gmail.com>
2018-08-23 13:02:17 +1000

    user token saved and navigates to main page

--- src/models/CurrentUser.js
+++ src/models/CurrentUser.js
@@ -11,22 +11,26 @@ const UserInfo = types.model('UserInfo', { _id: types.identifier,  firstName: ty
 export const CurrentUser = types
   .model('CurrentUser', {    authToken: types.maybe(types.string),    info: types.maybe(UserInfo),  })
   .actions(self => 
-    ({  /* setupAuth: flow(function*() {  yield self.getAuthToken();   yield self.getUserInfo();  }),
-         getAuthToken: flow(function*() 
+    ({   setupAuth: flow(function*() //5--last
+            {  yield self.getAuthToken();   
+                yield self.getUserInfo();  
+            }),
+         getAuthToken: flow(function*() //3
             { try {  const token = yield AsyncStorage.getItem(TOKEN_KEY);
+                console.log('/models/currentuser.js-getAuthToken-token=',token);
                     if (token) {  self.authToken = token;  } 
                     else {  NavigationService.navigate('Auth');   }
                    } 
                catch (error) { console.log('getAuthToken-error', error);  }
             }),
-            */
-         saveToken: flow(function*(token) 
+            
+         saveToken: flow(function*(token) //2
             {  try {   console.log('/models/currentuser.js--saveToken');
                     yield AsyncStorage.setItem(TOKEN_KEY, token);
                 } catch (error) {  console.log('/models/cuser-saveToken--error', error); }
             }),
             
-         login: flow(function*(providerToken, provider) 
+         login: flow(function*(providerToken, provider) //1
            {  try {   const res = yield customersApi.post({ token: providerToken, provider, }).json(); //--Api.js--------trigger customerApi
                       console.log('/models/currentuser.js--login---res=',res);
                      if (res.token) {   self.authToken = res.token;
@@ -35,10 +39,12 @@ export const CurrentUser = types
                              }
                   } catch (error) {   console.log('/models/currentuser.js--login--error', error); }
            }),
-        getUserInfo: flow(function*() 
+        getUserInfo: flow(function*() //4 //-----------------------here its going to server to get user info with jwt token
         {  try { if (self.authToken) 
                     {  const res = yield customersApi.url('/me').headers({ Authorization: `Bearer ${self.authToken}` }).get().json();
+                    console.log('/models/currentuser.js--getUserInfo--res=', res);
                        self.info = res;
+                       console.log('/models/currentuser.js--getUserInfo--self.info=', self.info); //--normalizes to userinfo defined at top
                        NavigationService.navigate('Main');
                     }
         } catch (error) { console.log('getUserInfo-error', error);   }
--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { Box, Text } from 'react-native-design-utility';
 import OnboardingLogo from '../commons/OnboardingLogo';
+import { inject } from 'mobx-react/native';
 
-
+@inject('currentUser')
 class SplashScreen extends Component 
 {   state={};
     componentDidMount() {   this.checkAuth();  }
-    checkAuth = () => {    setTimeout(() => {  this.props.navigation.navigate('Auth');  }, 1000);    //check jwt token
+    checkAuth = async () => {  //  setTimeout(() => {  this.props.navigation.navigate('Auth');  }, 1000);    //check jwt token
+                        await this.props.currentUser.setupAuth();
+                        // setTimeout(() => { await this.props.currentUser.setupAuth();  }, 1000);    //check jwt token
                      };
     render() 
     {     console.log('/SplashScreen-this.props',this.props);
