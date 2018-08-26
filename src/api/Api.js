ccd3b90082f85c645ef8917f3871ce1b0971844c
manojkmishra <manojmishra571980@gmail.com>
2018-08-27 09:47:19 +1000

    downgraded mobx-- bug--symbol not defined removed

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,4 +1,6 @@
 import wretch from 'wretch';
+import { BASE_URL } from '../constants';
 
 //export const customersApi = wretch('http://localhost:3000/api/v1/customers');
-export const customersApi = wretch('http://10.0.2.2:3000/api/v1/customers');
+//export const customersApi = wretch('http://10.0.2.2:3000/api/v1/customers');
+export const customersApi = wretch(`${BASE_URL}/customers`);
