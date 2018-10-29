d25fdc30758c6986eb871faae8bfa92e49e4cc18
manojkmishra <manojmishra571980@gmail.com>
2018-10-30 08:46:32 +1100

    categorycard component created and imported in homescreen, images also added in assets folder

---
+++ src/components/CategoryCard.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react';
+import { Box, Text } from 'react-native-design-utility';
+
+class CategoryCard extends PureComponent {
+  state = {};
+  render() 
+  {    
+    return (            
+          <Box >
+            <Text > CategoryCardComponent  </Text>
+          </Box>  
+    );
+  }
+}
+export default CategoryCard;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
 import { Box, Text } from 'react-native-design-utility';
-import { StatusBar } from 'react-native';
+import { StatusBar, FlatList } from 'react-native';
+import CategoryCard from '../components/CategoryCard';
+
+const categories = [
+  { id: 1, title: 'Grocery', image: require('../../assets/img/cart.png'), },
+  { id: 2, title: 'Drugs', image: require('../../assets/img/drugs.png'), },
+  { id: 3, title: 'Pets', image: require('../../assets/img/pets.png'), },
+  { id: 4, title: 'video games',  },
+];
+
+const NUM_COLUMNS = 3;
 
 class HomeScreen extends Component 
 {   static navigationOptions = {  title: 'HomeTitle',  };
   
     state={};
+    renderItem = ({ item }) => < CategoryCard {...item}/>
     render() 
     {    console.log('/HomeScreen-this.props',this.props);
+
       return (
-        <Box f={1} center >
+        <Box f={1} >
           <StatusBar barStyle="light-content" />
           <Box h={130} bg="red" w={1}>
-        
+            <Text>HomeScreen</Text>
           </Box>
-          <Text>HomeScreen</Text>
+          <Box f={1} bg="blue" >
+            <FlatList
+              data={categories}
+              renderItem={this.renderItem}
+            // numColumns={NUM_COLUMNS}
+             />
+          </Box>
+         
         </Box>
       );
     }
