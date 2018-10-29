67008ea152736c583e2ffb14d10f3d2df998f836
manojkmishra <manojmishra571980@gmail.com>
2018-10-30 08:21:52 +1100

    box created in middle of homescreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -10,7 +10,10 @@ class HomeScreen extends Component
     {    console.log('/HomeScreen-this.props',this.props);
       return (
         <Box f={1} center >
-           <StatusBar barStyle="light-content" />
+          <StatusBar barStyle="light-content" />
+          <Box h={130} bg="red" w={1}>
+        
+          </Box>
           <Text>HomeScreen</Text>
         </Box>
       );
